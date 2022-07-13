5fd55af5636e4eb2eed45645af89014504dd9716
dovydasvaliokas <74957396+dovydasvaliokas@users.noreply.github.com>
2022-07-13 12:01:09 +0300

    Uzseivinti failai

--- ViltiesZiedasFilmai/Models/Film.cs
+++ ViltiesZiedasFilmai/Models/Film.cs
@@ -2,5 +2,18 @@
 {
     public class Film
     {
+        private VZContext context;
+
+        public int FilmId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public int ReleaseYear { get; set; }
+
+        public int Length { get; set; }
+
+        public string Rating { get; set; }
     }
 }
--- ViltiesZiedasFilmai/Models/VZContext.cs
+++ ViltiesZiedasFilmai/Models/VZContext.cs
@@ -1,6 +1,51 @@
-﻿namespace ViltiesZiedasFilmai.Models
+﻿using MySql.Data.MySqlClient;
+
+namespace ViltiesZiedasFilmai.Models
 {
     public class VZContext
     {
+        public string ConnectionString { get; set; }
+
+        public VZContext(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+
+        private MySqlConnection GautiJungti()
+        {
+            return new MySqlConnection(ConnectionString);
+        }
+
+
+        public List<Film> GautiVisusFilmus()
+        {
+            List<Film> list = new List<Film>();
+
+            using (MySqlConnection jungtis = GautiJungti())
+            {
+                jungtis.Open();
+                MySqlCommand sqlUzklausa = new MySqlCommand("SELECT * FROM film", jungtis);
+
+                using (MySqlDataReader skaitytuvas = sqlUzklausa.ExecuteReader())
+                {
+                    while (skaitytuvas.Read())
+                    {
+                        Film laikinasFilmas = new Film()
+                        {
+                            FilmId = skaitytuvas.GetInt32("film_id"),
+                            Title = skaitytuvas.GetString("title"),
+                            Description = skaitytuvas.GetString("description"),
+                            ReleaseYear = skaitytuvas.GetInt32("release_year"),
+                            Length = skaitytuvas.GetInt32("length"),
+                            Rating = skaitytuvas.GetString("rating"),
+                        };
+
+                        list.Add(laikinasFilmas);
+                        
+                    }
+                }
+            }
+            return list;
+        }
     }
 }
